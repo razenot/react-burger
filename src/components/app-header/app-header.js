54a38decefc865325c89a247739b91701f64c5ef
razenot <vadimrazumniy@gmail.com>
2022-09-11 20:14:59 +0400

    Последние правки

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './header.module.css';
 import globalStyles from './../../global.module.css';
@@ -6,14 +5,14 @@ import globalStyles from './../../global.module.css';
 function AppHeader() {
   return (
 	<div className={styles.headerWrapper}>
-		<div className={globalStyles.container}>
+		<div className={`${globalStyles.container} ${globalStyles.blackGrayBG}`}>
 			<header className={styles.header}>
 				<section className={styles.columnLeft}>
 					<div className={styles.button}>
 						<BurgerIcon type="primary" />
 						<span className='text text_type_main-default ml-2'>Конструктор</span>
 					</div>
-					<div className={styles.button + ' ml-2'}>
+					<div className={`${styles.button} ml-2`}>
 						<ListIcon type="secondary" />
 						<span className='text text_type_main-default text_color_inactive ml-2'>Лента заказов</span>
 					</div>
